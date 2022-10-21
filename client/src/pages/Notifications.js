7c13ec78b53ad302ede33dae314250374cfde297
johnnn-117 <hao.junh@northeastern.edu>
2022-10-21 14:41:07 -0700

    Johnson Notification part 10/21

---
+++ client/src/pages/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+function Notifications() {
+    return (
+        <Layout>
+            <h1 className='page-title'>Notifications</h1>
+        </Layout>
+    )
+
+}
+
+export default Notifications
