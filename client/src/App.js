0dad137db3099b2f46cd9971bec5b5a99c7a3667
Jing Feng <flora@Jings-MacBook-Pro-2.local>
2022-10-21 00:24:49 -0700

    Login and Registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { Button } from 'antd'
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Furry Vet Appointment Platform</h1>
-    </div>
+    <BrowserRouter>
+      <Toaster position='top-center' reverseOrder={false} />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 require('dotenv').config()
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const userRoute = require('./routes/userRoute');
+app.use('/api/user', userRoute);
 const port = process.env.PORT || 3000;
 
-console.log(process.env.MONGO_URL)
-app.listen(port, () => {console.log(`Node server started at port ${port}`)});
+// console.log(process.env.MONGO_URL)
+app.listen(port, () => { console.log(`Node server started at port ${port}`) });
