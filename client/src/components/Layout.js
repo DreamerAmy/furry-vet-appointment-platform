2789bdfc20b0a1749d21473b0e9340ee3f8f048f
Yu Fu <fu.yu4@northeastern.edu>
2022-10-21 13:03:26 -0700

    update code

---
+++ client/src/components/Layout.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "../layout.css";
+
+function Layout({ children }) {
+  return (
+    <div className="main">
+      <div className="d-flex layout">
+        <div className="sidebar">
+        sidebar
+
+    
+        </div>
+
+        <div className="content">
+          <div className="header">
+           header
+          </div>
+
+          <div className="body">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout
