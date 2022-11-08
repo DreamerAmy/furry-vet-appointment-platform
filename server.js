c5231fe217d8ed0349cc98f3e49568a9e5a7391c
jianweibi <77766857+jianweibi@users.noreply.github.com>
2022-11-08 00:08:17 -0800

    Completed doctor profile update

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorsRoute");
 
 app.use("/api/user", userRoute);
-app.use('/api/admin', adminRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
 const port = process.env.PORT || 3000;
 
 // console.log(process.env.MONGO_URL)
