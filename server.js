43f3616d600ffc79787ac6d5ba4778b21060830d
amyxu <67804304+DreamerAmy@users.noreply.github.com>
2022-12-06 20:09:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const dbConfig = require("./config/dbConfig");
+
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
-const path = require("path");
+
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-if (process.env.NODE_ENV) {
-  app.use("/", express.static('client/build'));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // });
-}
 const port = process.env.PORT || 3001;
 // console.log(process.env.MONGO_URL)
 app.get("/", (req, res) => res.send("Hello World!"));
