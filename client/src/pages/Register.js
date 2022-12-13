c5ef141b8b34bf0987871d67ca8f80d125b7c20d
Jing0001 <feng.jing1@northeastern.edu>
2022-12-12 20:16:34 -0800

    add authentication

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -35,7 +35,10 @@ function Register() {
             <Input placeholder="Name" />
           </Form.Item>
           <Form.Item label="Email" name="email">
-            <Input placeholder="Email" />
+            <Input placeholder="Email"
+              type="email"
+              pattern='^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$'
+            />
           </Form.Item>
           <Form.Item label="Password" name="password">
             <Input placeholder="Password" type="password" />
